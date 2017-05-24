f1d433a7952dd9d7960b185abfb98ea3ea3ce7ad
ashishdommety <ashishuiux@gmail.com>
2017-05-24 15:21:11 -0700

    Fully functional.. just need to add some extra effects!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,171 +1,251 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-//object of arrays with the questions and options
-var questions = [
-  {q1:'What does HTML stand for?',
-    a1:'Hypertext Markup Language',
-    a2:'Hypertext Margin Language',
-    a3:'Historic Markup Literacy',
-    a4:'Historic Makeup Language'
+  //object of arrays with the questions and options
+  var questions = [{
+      q1: 'What does HTML stand for?',
+      a1: 'Hypertext Markup Language',
+      a2: 'Hypertext Margin Language',
+      a3: 'Historic Markup Literacy',
+      a4: 'Historic Makeup Language'
+    },
+    {
+      q2: 'Who created Javascript?',
+      a1: 'Brendan Eich',
+      a2: 'John Resig',
+      a3: 'James Gosling',
+      a4: 'Bjarne Stroustrup'
+    },
+    {
+      q3: 'Which of these is not a CSS framework?',
+      a1: 'Angular',
+      a2: 'LESS',
+      a3: 'Bulma',
+      a4: 'SASS'
+    },
+    {
+      q4: 'Which company created bootstrap?',
+      a1: 'Twitter',
+      a2: 'Facebook',
+      a3: 'Google',
+      a4: 'Whatsapp'
+    },
+    {
+      q5: 'Which is these is wrong syntax?',
+      a1: 'click.element = function(){code goes here}',
+      a2: "$(element).on('click', function(){code goes here})",
+      a3: 'variable.onclick = function(){code goes here}',
+      a4: '$(element).click(function(){code goes here})'
+    },
+    {
+      q6: 'What is the first step to take before writing a program?',
+      a1: 'Psuedocode',
+      a2: 'Declare all the varaibles you might need',
+      a3: 'Google other peoples solutions',
+      a4: 'Open your text editor'
+    },
+    {
+      q7: 'What is the purpose of SSL?',
+      a1: 'Security',
+      a2: 'Performance',
+      a3: 'Security and Performance',
+      a4: 'For responsiveness'
+    },
+    {
+      q8: 'Which of these is not a Javascript Framework?',
+      a1: 'Haml',
+      a2: 'Angular',
+      a3: 'React',
+      a4: 'Vue'
+    },
+    {
+      q9: 'Which engine does Google Chrome run on?',
+      a1: 'V8',
+      a2: 'V6',
+      a3: 'V4',
+      a4: 'V9'
+    },
+    {
+      q10: 'Who was the first programmer?',
+      a1: 'Ada Lovelace',
+      a2: 'Herman Hollerith',
+      a3: 'Alan Turing',
+      a4: ' Konrad Zuse'
+    }
+  ]
+
+  var answerDescip = [{
+    ad1:'HTML stands for Hypertext Markup Language'
   },
-  {q2:'Who invented Javascript?',
-    a1:'Brendan Eich',
-    a2:'John Resig',
-    a3:'James Gosling',
-    a4:'Bjarne Stroustrup'
+  {
+    ad2:'Brendan Eich created Javascript in a span of 10 days in May 1995'
   },
-  {q3:'Which of these is not a CSS framework?',
-      a1:'Angular',
-      a2:'LESS',
-      a3:'Bulma',
-      a4:'SASS'
+  {
+    ad3:'Angular JS is not a CSS framework, but it is a Javascript framework used to create single page applications'
   },
-  {q4:'Which company invented bootstrap?',
-    a1:'Twitter',
-    a2:'Facebook',
-    a3:'Google',
-    a4:'Whatsapp'
+  {
+    ad4:'Mark Otto and Jacob Thorton created bootstrap at Twitter, and originally called it "Twitter Blueprint" '
   },
-  {q5:'Which is these is wrong syntax?',
-    a1:'click.element = function(){code goes here}',
-    a2:"$(element).on('click', function(){code goes here})",
-    a3:'variable.onclick = function(){code goes here}',
-    a4:'$(element).click(function(){code goes here})'
+  {
+    ad5:'"click.element = function(){code goes here}" is the wrong syntax to write a click-function'
   },
-  {q6:'What is the first step to take before writing a program?',
-    a1:'Psuedocode',
-    a2:'Declare all the varaibles you might need',
-    a3:'Google other peoples solutions',
-    a4:'Open your text editor'
+  {
+    ad6:'The first step to take before writing a program, is to Psuedocode. It is an informal language that helps programmers develop algorithms'
   },
-  {q7:'What is the purpose of SSL?',
-    a1:'Security',
-    a2:'Performance',
-    a3:'Security and Performance',
-    a4:'For responsiveness'
+  {
+    ad7:'SSL (Secure Sockets Layer) is the standard security technology for establishing an encrypted link between a web server and a browser.'
   },
-  {q8:'Which of these is not a Javascript Framework?',
-    a1:'Haml',
-    a2:'Angular',
-    a3:'React',
-    a4:'Vue'
+  {
+    ad8:'Haml is a markup language that’s used to cleanly and simply describe the HTML of any web document without the use of inline code.'
   },
-  {q9:'Which engine does Google Chrome run on?',
-    a1:'V8',
-    a2:'V6',
-    a3:'V4',
-    a4:'V9'
+  {
+    ad9:"V8 is Google's open source high-performance JavaScript engine and it is written in C++ "
   },
-  {q10:'Who was the first programmer?',
-    a1:'Ada Lovelace',
-    a2:'Herman Hollerith',
-    a3:'Alan Turing',
-    a4:' Konrad Zuse'
+  {
+    ad10:"Ada Lovelace was the first to recognise that the machine had applications beyond pure calculation, and created the first algorithm intended to be carried out by such a machine. "
   }
 ]
 
-//variables
-var counter;
-var rightGuesses;
-var wrongGuesses;
-var unanswered;
-var secondsLeft = parseInt($('#time').text());
-var timer;
+  //variables
+  var counter;
+  var rightGuesses;
+  var wrongGuesses;
+  var unAnswered;
+  var secondsLeft = parseInt($('#time').text());
+  var timer;
 
-//init function (counter, right guesses, wrong guesses, hides and shows )
-function init(){
-  counter = 0;
-  rightGuesses = 0;
-  wrongGuesses = 0;
-  unanswered = 0;
-  $('#play').hide();
-  $('#clock').hide();
-}
-init();
+  //init function (counter, right guesses, wrong guesses, hides and shows )
+  function init() {
+    counter = 0;
+    rightGuesses = 0;
+    wrongGuesses = 0;
+    noAnswer = 0;
+    $('#play').hide();
+    $('#clock').hide();
+    $('#check').hide();
+  }
+  init();
 
-//function for start screen
-$('#begin').on('click',function(){
+  //function for start screen
+  $('#begin').on('click', function() {
     $('#play').show();
     $('#clock').show();
     $('.start').hide();
     counter++;
     console.log(counter);
     gamePlay();
-});
+  });
 
-// NOTE: Errors so far: Everytime the timer runs out of time, the next counter adds 1 extra
-//than it needs too.
-function events(){
-  $('#opt1, #opt2, #opt3, #opt4').click(function(){
-    var check = $(this).data('ref');
-    if(check === 'correct'){
-      console.log('you guessed the right answer!');
-      rightGuesses++;
-      //function if correct guess
+
+
+
+  function events() {
+    $('#opt1, #opt2, #opt3, #opt4, #next').click(function() {
+      var check = $(this).data('ref');
+      $('#play').hide();
+      $('#check').show();
+      if (check === 'correct') {
+        rightAnswer();
+        // console.log('you guessed the right answer!');
+        // rightGuesses++;
+        //function if correct guess
+      } else if (check === 'wrong') {
+        wrongAnswer();
+        //function if wrong guess
+      }
+      else if(check === 'next'){
+        $('#play').show();
+        counter++;
+        console.log(counter);
+        checkEnd();
+        clearInterval(timer);
+        secondsLeft = 30;
+        timerStart();
+        $('#check').hide();
+      }
+      //function if time runs out
+    });
+  }
+
+  function gamePlay() {
+    questionSelection();
+    events();
+  }
+
+  //function to create each set of question and answers
+  function questionSelection() {
+    clearInterval(timer);
+    timerStart();
+    $('#question').text(counter + '. ' + questions[counter - 1]['q' + counter]);
+    for (var i = 1; i < 5; i++) {
+      $('#opt' + i).text(questions[counter - 1]['a' + i]);
     }
-    else if(check === 'wrong'){
-      console.log('you clicked the wrong answer');
-      wrongGuesses++;
-      //function if wrong guess
+  }
+  function writeUp(){
+    $('#description').text( answerDescip[counter-1]['ad' + counter]);
+  }
+
+  //function for end screen
+  function checkEnd() {
+    if (counter === questions.length + 1) {
+      clearInterval(timer);
+      $('#play').hide();
+      $('#clock').hide();
+      $('#check').hide();
+      $('#result').append('<h2>Correct Guesses: ' + rightGuesses + '</h2>');
+      $('#result').append('<h2>Wrong Guesses: ' + wrongGuesses + '</h2>');
+      $('#result').append('<h2>Unanswered: ' + noAnswer + '</h2>');
+      $('#result').addClass('white');
+      console.log('game ended');
+    } else {
+      questionSelection();
     }
-    counter++;
-    console.log(counter);
-    checkEnd();
+  }
+
+  //timer
+  function timerStart() {
+    timer = setInterval(function() {
+      if (secondsLeft === 0) {
+        $('#play').hide();
+        $('#check').show();
+        unAnswered();
+        counter++;
+        console.log(counter);
+        clearInterval(timer);
+        secondsLeft = 30;
+        checkEnd();
+      }
+      $('#time').text(secondsLeft);
+      secondsLeft--;
+    }, 1000);
+  }
+
+  function rightAnswer(){
     clearInterval(timer);
     secondsLeft = 30;
-    timerStart();
-    //function if time runs out
-  });
-}
-function gamePlay(){
-  questionSelection();
-  events();
-}
-
-//function to create each set of question and answers
-function questionSelection(){
-  clearInterval(timer);
-  timerStart();
-  $('#question').text(counter+'. '+questions[counter-1]['q'+counter]);
-  for(var i = 1; i < 5; i++){
-    $('#opt'+i).text(questions[counter-1]['a'+i]);
+    $('#answer').text('Correct!');
+    writeUp();
+    console.log('you guessed the right answer!');
+    rightGuesses++;
   }
-}
 
-//function for end screen
-function checkEnd(){
-  if(counter === questions.length+1){
-    $('#play').hide();
-    $('#clock').hide();
-    $('#result').append('<h2>Correct Guesses: '+ rightGuesses +'</h2>');
-    $('#result').append('<h2>Wrong Guesses: '+ wrongGuesses +'</h2>');
-    $('#result').append('<h2>unanswered: '+ unanswered +'</h2>');
-    console.log('game ended');
+  function wrongAnswer(){
+    clearInterval(timer);
+    secondsLeft = 30;
+    $('#answer').text('Wrong..');
+    writeUp();
+    console.log('you guessed the wrong answer!');
+    wrongGuesses++;
   }
-  else{
-    questionSelection();
+
+  function unAnswered(){
+    $('#answer').text('You ran out of time!');
+    writeUp();
+    console.log('you ran out of time!');
+    noAnswer++;
   }
-}
 
-//timer
-function timerStart(){
-   timer = setInterval(function(){
-    if(secondsLeft === 0){
-      console.log('you ran out of time!');
-      counter++;
-      console.log(counter);
-      secondsLeft = 30;
-      unanswered++;
-      checkEnd();
-    }
-    $('#time').text(secondsLeft);
-    secondsLeft--;
-  },1000);
-
-}
-// function rightAnswer(){
-//
-// }
-
-//end
+  //end
 });
+
+// TODO: Need to shuffle options so that the first one isn't always correct
+// TODO: Need to make sure timer doesn't keep running after you make a wrong guess
